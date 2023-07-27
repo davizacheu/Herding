f079004761bd9e3c279746b64867260dc2d3a4de
davizacheu <zacheudavi@gmail.com>
2023-07-27 16:17:01 -0700

    parcel_land class added

---
+++ Landparcel.py
@@ -0,0 +1,9 @@
+class Landparcel:
+    def __init__(self, vegetation, width, height, x_origin, y_origin):
+        self.vegetation = vegetation
+        self.width = width
+        self.height = height
+        self.x_origin = x_origin
+        self.y_origin = y_origin
+#
+
